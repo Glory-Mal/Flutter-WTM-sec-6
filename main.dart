a9598f324be392a16f49c847bfe3e50d463d9ae2
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-03 13:26:17 +0300

    style and email icon

--- main.dart
+++ main.dart
@@ -35,17 +35,35 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                   )),
               Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: Row(
                   children: <Widget>[
                     Icon(
                       Icons.phone,
-                      color: Colors.white,
-                      size: 50.0,
+                      color: Colors.teal,
                     ),
-                    Text('+255783435464')
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+255783435464',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
                   ],
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[Icon(Icons.email),],
+                ),
+              )
             ],
           ),
         ),
