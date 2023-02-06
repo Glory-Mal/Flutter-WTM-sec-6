4d83119e3a900ecee672f7a350d95692872fee00
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-06 22:53:57 +0300

    final mi card app
    containers for phone and email tabs changed to cards with list tiles.
    cards are customized already.

--- main.dart
+++ main.dart
@@ -8,10 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -34,45 +36,36 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'Source Sans Pro',
                     letterSpacing: 2.5,
                   )),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+255783435464',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      '+255783435464',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.email, color: Colors.teal),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'malamshaglory@gmail.com',
-                      style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          color: Colors.teal.shade900,fontSize:20.0),
-                    ),
-                  ],
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'malamshaglory@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0),
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
